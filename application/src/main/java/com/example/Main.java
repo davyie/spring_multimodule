34eaec12f80e4781f7518c69808702d80e87c8f0
David Yu <yu.davidcheng@gmail.com>
2024-11-17 21:54:30 +0100

    Unsuccessful attempt at connecting to MongoDB

--- application/src/main/java/com/example/Main.java
+++ application/src/main/java/com/example/Main.java
@@ -2,12 +2,19 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
-@ComponentScan(basePackages = {"com.example"})
+import java.util.Collections;
+
+@SpringBootApplication(scanBasePackages = "com.example")
+@EnableMongoRepositories(basePackages = "com.example")
 public class Main {
     public static void main(String[] args) {
-        SpringApplication.run(Main.class, args);
+
+        SpringApplication application =  new SpringApplication(Main.class);
+//        application.setDefaultProperties(Collections.singletonMap(
+//                "spring.data.mongodb.uri",
+//                "mongodb+srv://ims:N9ihlRBrzTlipQDZ@crochet-ims.lblyt.mongodb.net/?retryWrites=true&w=majority&appName=crochet-ims"));
+        application.run(args);
     }
 }
