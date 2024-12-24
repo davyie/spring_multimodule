b5ceeccc0acbee63c332f1f7cb401854a19d23f5
David Yu <yu.davidcheng@gmail.com>
2024-12-25 00:31:43 +0100

    Comment out JPA. Add rate limit config

--- application/src/main/java/com/example/Main.java
+++ application/src/main/java/com/example/Main.java
@@ -3,13 +3,13 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+//import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories(basePackages = "com.example")
 @EnableConfigurationProperties(RateLimitConfig.class)
-@EnableJpaRepositories
+//@EnableJpaRepositories
 public class Main {
     public static void main(String[] args) {
         SpringApplication application =  new SpringApplication(Main.class);
